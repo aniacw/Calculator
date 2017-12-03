6d0aeb79956751f32036af24addc99303bb09c58
aniacw <cwiek.ania@gmail.com>
2017-12-03 15:27:19 +0100

    First Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,19 @@
 package com.kodilla;
 
 public class Calculator {
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.add();
+        calculator.subtract();
+    }
+
+    public void add(){
+        System.out.println("Adding");
+
+    }
+
+    public void subtract(){
+        System.out.println("Subtracting");
+
+    }
 }
